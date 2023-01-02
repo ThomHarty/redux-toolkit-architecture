1d88ac953da040af5e5768fa48645b2407d7af5b
Thom Harty <thomharty@192.168.1.102>
2023-01-02 16:35:36 +0000

    Added List Products API

---
+++ src/store/products/listProducts/listProducts.api.ts
@@ -0,0 +1,14 @@
+import { ENDPOINT } from "../../../endpoint";
+import { ListProductsResponse } from "./listProducts.types";
+
+export const fetchListProducts = async (): Promise<ListProductsResponse> => {
+    const response = await fetch(`${ENDPOINT}/products`);
+
+    if(!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+    };
+
+    const data = await response.json();
+    return data;
+};
