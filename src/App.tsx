275f1bc15671989712acd6abef2c03e612758441
Thom Harty <thomharty@192.168.1.102>
2023-01-02 15:22:41 +0000

    Added Redux Store Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React, { ReactElement } from "react";
 import { View } from "react-native";
+import { Provider } from "react-redux";
+
+import { store } from "./store";
 
 export const App = (): ReactElement => {
-  return <View />
+  return (
+    <Provider store={store}>
+      <View />
+    </Provider>
+  );
 };
