159c65698da2acf67aba13480dd29ee36b384cef
Thom Harty <thomharty@192.168.1.102>
2023-01-02 20:20:33 +0000

    Added CreateProduct Component

---
+++ src/components/CreateProduct.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement, useEffect } from "react";
+import { View } from "react-native";
+
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { createProduct } from "../store/products";
+
+export const CreateProduct = (): ReactElement => {
+    const dispatch = useAppDispatch();
+    const { products, loading } = useAppSelector(state => state.products);
+
+    useEffect(() => {
+        dispatch(createProduct({
+            body: {
+                title: "test product",
+                price: 13.5,
+                description: "lorem ipsum set",
+                image: "https://i.pravatar.cc",
+                category: "electronic",
+            },
+        }));
+    }, []);
+
+    console.log("create product loading: ", loading);
+    console.log("products length: ", products.length);
+
+    return <View />;
+};
