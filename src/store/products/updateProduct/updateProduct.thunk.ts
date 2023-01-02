f59d9e93c6fb7715ef91a6178c54d3d6aa5d9dd8
Thom Harty <thomharty@192.168.1.102>
2023-01-02 20:36:44 +0000

    Added UpdateProduct Thunk

---
+++ src/store/products/updateProduct/updateProduct.thunk.ts
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+import { fetchUpdateProduct } from "./updateProduct.api";
+import { UpdateProductRequest, UpdateProductResponse } from "./updateProduct.types";
+
+export const updateProduct = createAsyncThunk(
+    "product/updateProduct",
+    async (updateProductRequest: UpdateProductRequest,): Promise<UpdateProductResponse> => {
+        const response = await fetchUpdateProduct(updateProductRequest);
+        return response;
+    },
+);
