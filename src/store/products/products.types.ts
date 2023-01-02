21ce10a4c7cf8339ab027a0b7fa3e76a9e98c39b
Thom Harty <thomharty@192.168.1.102>
2023-01-02 16:20:50 +0000

    Updated Product Interface

--- src/store/products/products.types.ts
+++ src/store/products/products.types.ts
@@ -1,7 +1,14 @@
 export interface IProduct {
     id: number;
-    name: string;
+    title: string;
+    price: number;
     description: string;
+    category: string;
+    image: string;
+    rating: {
+        rate: number;
+        count: number;
+    };
 };
 
 export interface IProductsState {
