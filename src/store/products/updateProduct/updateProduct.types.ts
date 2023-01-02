37527ffebeaafa69662830a156acac8f44988345
Thom Harty <thomharty@192.168.1.102>
2023-01-02 20:25:31 +0000

    Added UpdateProduct Types

---
+++ src/store/products/updateProduct/updateProduct.types.ts
@@ -0,0 +1,8 @@
+import { IProduct } from "../products.types";
+
+export interface UpdateProductRequest {
+    body: Omit<IProduct, "id">;
+    params: { id: number; };
+};
+
+export type UpdateProductResponse = IProduct;
