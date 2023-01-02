611fdc38495d9ca40a169a6cbfefed74ecaacee0
Thom Harty <thomharty@192.168.1.102>
2023-01-02 21:07:34 +0000

    Added Delete Product APi

---
+++ src/store/products/deleteProduct/deleteProduct.api.ts
@@ -0,0 +1,22 @@
+import { ENDPOINT } from "../../../endpoint";
+import { DeleteProductRequest, DeleteProductResponse } from "./deleteProduct.types";
+
+export const fetchDeleteProduct = async (
+    deleteProductRequest: DeleteProductRequest,
+): Promise<DeleteProductResponse> => {
+    const response = await fetch(`${ENDPOINT}/products/${deleteProductRequest.params.id}`, {
+        method: "DELETE",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+    });
+
+    if(!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+    };
+
+    const data = await response.json();
+    return data;
+};
